11a30458e05f7fde711ec13ca678afd5c40e92a3
Sharvil Naik <snn5@njit.edu>
2012-07-11 01:26:36 +0000

    'view' renderes automatically on data change in model

--- public/js/Tunes.js
+++ public/js/Tunes.js
@@ -1,7 +1,11 @@
 (function($) {
 	window.Album = Backbone.Model.extend({});
 	window.AlbumView = Backbone.View.extend({
+		tagName: 'li',
+		className: 'album',
 		initialize: function() {
+			_.bindAll(this, 'render');
+			this.model.bind('change', this.render);
 			this.template = _.template($('#album-template').html());
 		},
 		
