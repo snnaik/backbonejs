f4e11c5ee7fcf9fd91f046e97acee8f477e02c12
Sharvil Naik <snn5@njit.edu>
2012-07-11 01:11:10 +0000

    simple model/view

---
+++ public/js/Tunes.js
@@ -0,0 +1,14 @@
+(function($) {
+	window.Album = Backbone.Model.extend({});
+	window.AlbumView = Backbone.View.extend({
+		initialize: function() {
+			this.template = _.template($('#album-template').html());
+		},
+		
+		render: function() {
+			var renderedContent = this.template(this.model.toJSON());
+			$(this.el).html(renderedContent);
+			return this;
+		}
+	});
+})(jQuery);
